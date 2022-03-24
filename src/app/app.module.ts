845a8a82b5af6f8120bf568cda546a348f09761f
leandrocasio <leandrotkcasio@gmail.com>
2022-03-24 18:48:35 -0300

    cadastrar, entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EntrarComponent } from './entrar/entrar.component';
     MenuComponent,
     RodapeComponent,
     HomeComponent,
-    EntrarComponent
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
